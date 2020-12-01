927ee12bbe370917f911b6faff2f7147bb201ec7
Eli Ben-Michael <ebenmichael@berkeley.edu>
2020-12-01 16:02:50 -0500

    Changed calibration to only computing weights for non-empty cells

--- R/calibrate.R
+++ R/calibrate.R
@@ -24,8 +24,13 @@ calibrate <- function(formula, target_count, data,
                         order, lambda, prob_weights, verbose, ...)
 
   # combine back in and return
-  # return(weights)
-  cells %>% mutate(weight = weights) %>%
+  cells %>% filter(sample_count != 0) %>%
+    mutate(weight = weights) %>%
+    select(-sample_count, -target_count) %>%
+    right_join(cells,
+               by = cells %>% select(-sample_count, -target_count) %>%
+                    names()) %>%
+    mutate(weight = replace_na(weight, 0)) %>%
     return()
 }
 
@@ -40,9 +45,7 @@ create_cells <- function(formula, target_count, data) {
     summarise(sample_count = sum(!!sample_count),
               target_count = sum(!!target_count)) %>%
     ungroup()
-  
-  sample_counts <- cells$sample_count
-  target_counts <- cells$target_count
+
   return(cells)
   # cells <- cells %>% select(-sample_count, -target_count)
   # return(list(cells = cells, sample_counts = sample_counts,
@@ -61,7 +64,7 @@ calibrate_ <- function(cells, sample_counts, target_counts, order = NULL,
   constraints <- create_rake_constraints(cells, D, sample_counts, 
                                          target_counts, verbose)
 
-  
+  # return(constraints)
   # P matrix and q vector
   if(verbose) message("Creating quadratic term matrix")
   P <- create_rake_Pmat(D, sample_counts, lambda)
@@ -73,13 +76,16 @@ calibrate_ <- function(cells, sample_counts, target_counts, order = NULL,
 
   settings <- do.call(osqp::osqpSettings,
                         c(list(...), list(verbose = verbose)))
-
+  if(verbose) message("Setting up optimization")
+  solver <- osqp::osqp(P, qvec, constraints$A, constraints$l, constraints$u,
+                       pars = settings)
   if(verbose) message("Optimizing")
-  solution <- osqp::solve_osqp(P, qvec, constraints$A,
-                               constraints$l, constraints$u,
-                               pars = settings)
-  
-  return(solution$x[1:nrow(cells)])
+  # solution <- osqp::solve_osqp(P, qvec, constraints$A,
+  #                              constraints$l, constraints$u,
+  #                              pars = settings)
+  solution <- solver$Solve()
+  return(solution$x)
+  # return(solution$x[1:nrow(cells)])
 
 }
 
@@ -126,43 +132,52 @@ create_rake_constraints <- function(cells, D, sample_counts, target_counts,
   # number of cells
   n_cells <- nrow(cells)
   contrast_cells <- lapply(cells, contrasts, contrasts = F)
+  # number of non-empty cells
+  n_nempty <- sum(sample_counts != 0)
+  sample_counts_nempty <- sample_counts[sample_counts != 0]
 
   # sum to N constraint
   if(verbose) message("\tx Sum to N constraint")
-  A_sumN <- Matrix::cbind2(
-      Matrix::Matrix(sample_counts, nrow = 1, ncol = n_cells),
-      Matrix::Matrix(0, nrow = 1, ncol = ncol(D))
-      )
+  # A_sumN <- Matrix::cbind2(
+  #     Matrix::Matrix(sample_counts, nrow = 1, ncol = n_cells),
+  #     Matrix::Matrix(0, nrow = 1, ncol = ncol(D))
+  #     )
+  A_sumN <- Matrix::Matrix(sample_counts_nempty, nrow = 1, ncol = n_nempty)
   l_sumN <- sum(target_counts)
   u_sumN <- l_sumN
 
 
   # non-negative constraint
   if(verbose) message("\tx Non-negativity constraint")
-  A_nn <- Matrix::cbind2(Matrix::Diagonal(n_cells),
-                        Matrix::Matrix(0, nrow = n_cells, ncol = ncol(D)))
-  l_nn <- rep(0, n_cells)
-  u_nn <- rep(Inf, n_cells)
+  # A_nn <- Matrix::cbind2(Matrix::Diagonal(n_cells),
+  #                       Matrix::Matrix(0, nrow = n_cells, ncol = ncol(D)))
+  A_nn <- Matrix::Diagonal(n_nempty)
+  # l_nn <- rep(0, n_cells)
+  # u_nn <- rep(Inf, n_cells)
+  l_nn <- rep(0, n_nempty)
+  u_nn <- rep(Inf, n_nempty)
 
   # marginal constraints
   if(verbose) message("\tx Exact marginal balance constraints")
   design_mat <- Matrix::sparse.model.matrix(~ . - 1, cells, contrast_cells)
-  A_marg <- Matrix::cbind2(Matrix::t(design_mat * sample_counts),
-                          Matrix::Matrix(0, nrow = ncol(design_mat),
-                                         ncol = ncol(D)))
+  # A_marg <- Matrix::cbind2(Matrix::t(design_mat * sample_counts),
+  #                         Matrix::Matrix(0, nrow = ncol(design_mat),
+  #                                        ncol = ncol(D)))
+  A_marg <- Matrix::t(design_mat[sample_counts != 0, , drop = F] * 
+                      sample_counts_nempty)
   l_marg <- as.numeric(Matrix::t(design_mat) %*% target_counts)
   u_marg <- l_marg
 
   # pre-compute t(D) %*% w
-  if(verbose) message("\tx Pre-computing design matrix multiplication")
-  A_D <- Matrix::cbind2(Matrix::t(D * sample_counts), -Matrix::Diagonal(ncol(D)))
-  l_D <- numeric(ncol(D))
-  u_D <- numeric(ncol(D))
+  # if(verbose) message("\tx Pre-computing design matrix multiplication")
+  # A_D <- Matrix::cbind2(Matrix::t(D * sample_counts), -Matrix::Diagonal(ncol(D)))
+  # l_D <- numeric(ncol(D))
+  # u_D <- numeric(ncol(D))
 
   # combine
-  A <- rbind(A_sumN, A_nn, A_marg, A_D)
-  l <- c(l_sumN, l_nn, l_marg, l_D)
-  u <- c(u_sumN, u_nn, u_marg, u_D)
+  A <- rbind(A_sumN, A_nn, A_marg)#, A_D)
+  l <- c(l_sumN, l_nn, l_marg)#, l_D)
+  u <- c(u_sumN, u_nn, u_marg)#, u_D)
 
   return(list(A = A, l = l, u = u))
 
@@ -170,26 +185,29 @@ create_rake_constraints <- function(cells, D, sample_counts, target_counts,
 
 create_rake_Pmat <- function(D, sample_counts, lambda) {
 
-  # D <- D * sample_counts
-  # P <- D %*% Matrix::t(D)
-  # P <- Matrix::bdiag(lambda * Matrix::Diagonal(nrow(D)),
-  #                     Matrix::Diagonal(ncol(D)))
-  P <- Matrix::bdiag(c(as.list(rep(lambda, nrow(D))),
-                       as.list(rep(1, ncol(D)))))
-  # return(P + lambda * Matrix::Diagonal((nrow(D))))
+  # P <- Matrix::bdiag(c(as.list(rep(lambda, nrow(D))),
+  #                      as.list(rep(1, ncol(D)))))
+  nnz_idxs <- which(sample_counts != 0)
+  sqrtP <- D[nnz_idxs,, drop = F] * sample_counts[nnz_idxs]
+  P <- sqrtP %*% Matrix::t(sqrtP) + lambda * Matrix::Diagonal(length(nnz_idxs))
   return(P)
 
 }
 
 create_rake_qvec <- function(D, sample_counts, target_counts, lambda, prob_weights) {
 
+  nnz_idxs <- which(sample_counts != 0)
+
   if(is.null(prob_weights)) {
-    prob_weights <- numeric(nrow(D))
+    # prob_weights <- numeric(nrow(D))
+    prob_weights <- numeric(length(nnz_idxs))
   }
 
   rhs <- Matrix::t(D) %*% target_counts
-  lhs <- sample_counts * D
+  # lhs <- sample_counts * D
+  lhs <- D[nnz_idxs,, drop = F] * sample_counts[nnz_idxs]
 
-  return(-c(as.numeric(lhs %*% rhs) + lambda * prob_weights,
-            numeric(ncol(D))))
+  # return(-c(as.numeric(lhs %*% rhs) + lambda * prob_weights,
+  #           numeric(ncol(D))))
+  return(-c(as.numeric(lhs %*% rhs) + lambda * prob_weights))
 }
